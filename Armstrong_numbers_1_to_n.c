4b73b6acf96957e5939d911698d2b6a6907fd3e0
CoderSid007 <siddhesdas2020@gmail.com>
2023-07-13 19:57:53 +0530

    first commit

---
+++ Armstrong_numbers_1_to_n.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  int n, i, temp, digit, sum, count = 0;
+
+  printf("Enter the upper limit: ");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++) {
+    temp = i;
+    sum = 0;
+    while (temp > 0) {
+      digit = temp % 10;
+      sum += digit * digit * digit;
+      temp /= 10;
+    }
+
+    if (sum == i) {
+      printf("%d ", i);
+      count++;
+    }
+  }
+
+  printf("\nTotal Armstrong numbers: %d\n", count);
+
+  return 0;
+}
